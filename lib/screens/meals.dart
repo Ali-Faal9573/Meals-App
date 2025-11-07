8041cebe462e4d7876bdfab8ec3791617c1a86c4
Ali-Faal <faal.ali.faal@gmail.com>
2025-11-07 20:17:58 +0330

    add params to MealsScreen widget

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  const MealsScreen({super.key , required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body:
+    );
   }
 }
