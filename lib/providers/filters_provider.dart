ec7f41697ff138fa50f9dcf38d8b194ccc074222
Ali-Faal <faal.ali.faal@gmail.com>
2025-11-12 18:13:52 +0330

    add second method for set the Filters

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -2,8 +2,8 @@ import 'package:flutter_riverpod/legacy.dart';
 
 enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
 
-class FiltersProviderNotifier extends StateNotifier<Map<Filter, bool>> {
-  FiltersProviderNotifier()
+class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
+  FiltersNotifier()
     : super({
         Filter.glutenFree: false,
         Filter.lactoseFree: false,
@@ -11,11 +11,16 @@ class FiltersProviderNotifier extends StateNotifier<Map<Filter, bool>> {
         Filter.vegan: false,
       });
 
+  void setFilters(Map<Filter, bool> chosenFilters) {
+    state = chosenFilters;
+  }
+
   void setFilter(Filter filter, bool isActive) {
     state = {...state, filter: isActive};
   }
 }
 
-final filtersProvider = StateNotifierProvider(
-  (ref) => FiltersProviderNotifier(),
-);
+final filtersProvider =
+    StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
+      (ref) => FiltersNotifier(),
+    );
